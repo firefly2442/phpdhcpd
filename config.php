4d7b4d5a944ad14535570b151bfddf8397480b43
Patrick <firefly2442@gmail.com>
2011-09-03 17:14:36 -0500

    update config file

--- config.php
+++ config.php
@@ -5,12 +5,17 @@
 $dhcpd_leases_file = "/var/lib/dhcp3/dhcpd.leases";
 
 //If password is left blank, NO authentication will
-//be required.  However, if it is NOT blank, the only
+//be required and the page will be visible to anyone.
+//However, if it is NOT blank, the only
 //way to view the page is by entering the correct password.
 //This password is in cleartext, if other people can access
 //this file, this is NOT secure.  For true security, use
-//Apache digest authentication or other methods.
+//Apache digest authentication or other methods such as
+//an htaccess file.
 $password = "";
 
 
+//phpdhcpd version
+//You don't have to change this
+$version = "0.5";
 ?>
